ecf52253e7809cc8f2b65fe2b1a88f76688e82a5
JayasuryaThirumaran <jayasurya.t@talentsprint.com>
2025-03-29 08:50:33 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,32 +1,15 @@
 
-import pandas as pd
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, f1_score
-
 def preprocess_data(input_path: str, output_path: str):
     """Loads data, cleans it, and saves the processed version."""
     df = pd.read_csv(input_path)
-    
-    # Drop Customer ID column if present
 
-    df.drop(columns=['CustomerId','RowNumber'], inplace=True)
-    
     # Handling missing values
-    df.fillna(df.median(numeric_only=True), inplace=True)
-    
-    # Handling categorical variables
-    label_encoders = {}
-    for col in df.select_dtypes(include=['object']).columns:
-        le = LabelEncoder()
-        df[col] = le.fit_transform(df[col])
-        label_encoders[col] = le
-    
-    # Save processed data
-    df.to_csv(output_path, index=False)
-    print(f'Processed data saved to {output_path}')
+    # ToDo
 
+    # Handling categorical variables
+    # ToDo
+
+    # Save processed data
+    # ToDo
 if __name__ == "__main__":
-    preprocess_data("data/customer_churn.csv", "data/processed_data.csv")
+    preprocess_data("Raw data path", "path to store processed data")
